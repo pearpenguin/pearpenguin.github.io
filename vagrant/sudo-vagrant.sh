1be0e80dadf514c74cc6e669de3d10746b56fa0a
Kenley Cheung <winnt253@hotmail.com>
2014-02-22 01:54:24 -0800

    Intermediary fixes for failed bootstrap.sh New sudo-vagrant.sh for commands that must be run as vagrant user (e.g. git config) debconf-set-selections from debconf file, provisioner doesn't have stdin

---
+++ vagrant/sudo-vagrant.sh
@@ -0,0 +1,6 @@
+# configure git for 'vagrant' user
+git config --global user.name "Kenley Cheung"
+git config --global user.email "winnt253@hotmail.com"
+git config --global core.editor /usr/bin/vi
+git config --global push.default simple
+git config --global core.excludesfile $home_dir/.gitignore
